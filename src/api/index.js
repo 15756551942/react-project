b71b783e0a6a0fd7909f6e45b3d358ef44befdc1
lance10030 <lance10030@163.com>
2020-10-29 17:29:00 +0800

    dev proxy config change

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import ajax from './ajax'
 
 const BASE = 'api'
 
-export const reqLogin = (name,pwd) => ajax(BASE + '/a/login;',{name,pwd},'POST')
+export const reqLogin = (name,pwd) => ajax(BASE + '/a/login',{name,pwd},'POST')
 
 // export const reqsearch = () => ajax(BASE + '/main/article-manage')
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,16 +3,21 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
 
   app.use('/api', createProxyMiddleware({
-    target: 'http://localhost:3000',
+    // target: 'http://106.75.15.3:7001/',
+    // target: 'http://39.107.82.218:30500/',
+    target: 'http://dev.admin.carrots.ptteng.com/',
     changeOrigin: true,
-  }));
+    secure: false,
+    pathRewrite: {
+      "^/api": ""
+    }}));
 
-  app.use('/api2', createProxyMiddleware({
-    target: 'http://dev.admin.carrots.ptteng.com',
-    changeOrigin: true,
-    pathRewrite: { //路径替换
-      '^/api2': '/api', // axios 访问/api2 == target + /api
-    }
-  }));
+  // app.use('/api2', createProxyMiddleware({
+  //   target: 'http://dev.admin.carrots.ptteng.com',
+  //   changeOrigin: true,
+  //   pathRewrite: { //路径替换
+  //     '^/api2': '/api', // axios 访问/api2 == target + /api
+  //   }
+  // }));
 
 };
